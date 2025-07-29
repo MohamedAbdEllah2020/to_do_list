73b28c3e21bd2646bdf6ab8ea618cb036c8dd8b5
MohamedAbdEllah2020 <eng.mohamed.abdellh2020@gmail.com>
2025-07-29 15:45:25 +0300

    firebase connection

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:to_do_list/firebase_options.dart';
 
-void main(List<String> args) {
+void main(List<String> args) async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(MyApp());
 }
 
